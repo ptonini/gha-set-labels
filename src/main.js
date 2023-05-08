679c498fdc7dfa3de5879712045fcc0cbb983d83
ptonini <pedro.tonini@hotmail.com>
2023-05-08 13:12:26 -0300

    chore: added overwrite option

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 require('dotenv-expand').expand(require('dotenv').config())
 const core = require("@actions/core");
-const {GitHub, context} = require("@actions/github");
+const {context} = require('@actions/github')
+const {Octokit} = require('@octokit/rest')
 
 async function main() {
-    const octokit = new GitHub(core.getInput('github_token'));
+    const octokit = new Octokit({auth: core.getInput('github_token')});
     const {owner, repo} = context.repo;
     const issue_number = context.payload.number
-    const labels = core.getInput("labels").split("\n").filter((x) => x !== "");
-    labels.forEach(label => core.info(`adding label: ${label}`))
+    const labels = core.getInput("labels").split("\n").filter((x) => x !== "")
+    const overwrite = core.getInput('overwrite') === 'true'
+    overwrite && await octokit.issues.removeAllLabels({owner, repo, issue_number})
+    core.info(`adding labels: ${labels.join('; ')}`)
     await octokit.issues.addLabels({owner, repo, labels, issue_number});
 }
 
